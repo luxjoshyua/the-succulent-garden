969f682157999550a844364a48d86b9d26fe5832
Joshua Fielding <josh.e.fielding@gmail.com>
2019-01-11 08:41:43 +1100

    morning svg work

---
+++ js/svg-animation.js
@@ -0,0 +1,14 @@
+// sunshine at the top
+anime({
+  targets: '#sunshine',
+  rotate: '1turn',
+  delay: (el, i) => i * 100,
+  duration: 2000,
+  loop: true,
+  direction: 'alternate',
+  easing: 'easeInOutSine'
+})
+
+
+// use one of the pulse grow things like in racquettes 
+
