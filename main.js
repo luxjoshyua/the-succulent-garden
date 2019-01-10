0a7519467bb9a3148b8b157881bd79827839161f
Your Name <josh.e.fielding@gmail.com>
2019-01-10 20:36:04 +1100

    beginnings

---
+++ main.js
@@ -0,0 +1,32 @@
+$('.menu-toggle').on('click', function() {
+  $('.menu').toggleClass('open')
+  return false  
+})
+
+
+// ==== marquee effect ====
+function makeMarquee(list) {
+  // this is now taking a list of things (see below)
+  // pick the marquees in the code
+  // slightly diff than before due to querySelectorAll as 
+  // this is a list of ALL marquees, not just the first one
+  const marquees = document.querySelectorAll('.marquee span')
+  
+  // for each thing in the array below, loop over it
+  list.forEach((title, index) => {
+    // fill up with the individual text (e.g. FIFTY, then Rik, then SuperHi)
+    const marqueeText = new Array(90).fill(title).join(' — ')
+    
+    // pick the 1st, then 2nd, ... version of the marquees from above
+    const marquee = marquees[index]
+    
+    // add the text!
+    marquee.innerHTML = marqueeText
+  })
+}
+// here we actually run our makeMarquee function, but now passing it
+// an array of things to fill up each marquee
+makeMarquee([
+  'Come and see our Succulent Garden! Open every day from 9am! Fun for the whole family',
+])
+
