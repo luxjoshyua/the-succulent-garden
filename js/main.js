e9e495e6aeebf4acece571d8a0fe7c8fbb755ccd
Your Name <josh.e.fielding@gmail.com>
2019-01-20 09:17:48 +1100

    cirlces

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$('.menu-toggle').on('click', function() {
+$('.menu-toggle').on('click', function () {
   $('.menu').toggleClass('open')
-  return false  
+  return false
 })
 
 
@@ -25,104 +25,127 @@ function makeMarquee() {
 makeMarquee()
 
 
-
-
 // only use e for jQuery events
 
 // arrojadoa
 
 var i = document.getElementById('ar-text');
 
-i.onmouseover = function() {
+i.onmouseover = function () {
   // console.log("rollover"); 
   document.getElementById('aj-text-b').style.display = 'block';
 }
 
-i.onmouseout = function() {
+i.onmouseout = function () {
   document.getElementById('aj-text-b').style.display = 'none';
-}  
+}
 
 // bunny ears
 
 var i = document.getElementById('be-text');
 
-i.onmouseover = function() {
+i.onmouseover = function () {
   // console.log("rollover"); 
   document.getElementById('be-text-b').style.display = 'block';
 }
 
-i.onmouseout = function() {
+i.onmouseout = function () {
   document.getElementById('be-text-b').style.display = 'none';
-}  
+}
 
 
 // grusonii
 
 var i = document.getElementById('gr-text');
 
-i.onmouseover = function() {
+i.onmouseover = function () {
   // console.log("rollover"); 
   document.getElementById('gr-text-b').style.display = 'block';
 }
 
-i.onmouseout = function() {
+i.onmouseout = function () {
   document.getElementById('gr-text-b').style.display = 'none';
-}  
+}
 
 // san pedro
 
 var i = document.getElementById('sa-text');
 
-i.onmouseover = function() {
+i.onmouseover = function () {
   // console.log("rollover"); 
   document.getElementById('sa-text-b').style.display = 'block';
 }
 
-i.onmouseout = function() {
+i.onmouseout = function () {
   document.getElementById('sa-text-b').style.display = 'none';
-}  
+}
 
 // peyote 
 
 var i = document.getElementById('pe-text');
 
-i.onmouseover = function() {
+i.onmouseover = function () {
   // console.log("rollover"); 
   document.getElementById('pe-text-b').style.display = 'block';
 }
 
-i.onmouseout = function() {
+i.onmouseout = function () {
   document.getElementById('pe-text-b').style.display = 'none';
-}  
+}
 
 // elephant 
 
 var i = document.getElementById('el-text');
 
-i.onmouseover = function() {
+i.onmouseover = function () {
   // console.log("rollover"); 
   document.getElementById('el-text-b').style.display = 'block';
 }
 
-i.onmouseout = function() {
+i.onmouseout = function () {
   document.getElementById('el-text-b').style.display = 'none';
-}  
+}
 
 
 // eulychnia 
 
 var i = document.getElementById('eu-text');
 
-i.onmouseover = function() {
+i.onmouseover = function () {
   // console.log("rollover"); 
   document.getElementById('eu-text-b').style.display = 'block';
 }
 
-i.onmouseout = function() {
+i.onmouseout = function () {
   document.getElementById('eu-text-b').style.display = 'none';
-}  
+}
 
 
 
 
+// circles here
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
+// here we grab all our the .circles from the html
+const circles = document.querySelectorAll('.circle')
+
+// circles returns us an array so we need to loop through it
+// inside of the forEach we get access to each individual element
+// along with its index
+circles.forEach((circle, index) => {
+  // in here we get access to each one as 'circle'
+  circle.animate([{
+    transform: 'scale(1)'
+  }, {
+    transform: 'scale(1.5)'
+  }, {
+    transform: 'scale(1)'
+  }], {
+    // here we use the index to create a staggered animation delay
+    delay: 400 * index,
+    duration: 4500,
+    iterations: Infinity
+  })
+})
